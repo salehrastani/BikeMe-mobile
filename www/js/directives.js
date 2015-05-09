c594ead866b00e06cd0615604a98d61bde6e74e1
Saleh Rastani <salehrastani@gmail.com>
2015-05-09 16:12:42 -0700

    fix indentation and formatting

--- www/js/directives.js
+++ www/js/directives.js
@@ -10,7 +10,6 @@ app.directive('map', function() {
         navigator.geolocation.getCurrentPosition(function (position) {
           $scope.position = position;
 
-
           var mapOptions = {
             center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
             zoom: 16,
@@ -19,6 +18,7 @@ app.directive('map', function() {
             mapTypeControl: false,
             streetViewControl: false,
           };
+
           var map = new google.maps.Map($element[0], mapOptions);
           var image = {
             url: "../img/motor-bike-64.png",
@@ -26,29 +26,29 @@ app.directive('map', function() {
           // origin: new google.maps.Point(0, 0),
           // anchor: new google.maps.Point(17, 34),
           scaledSize: new google.maps.Size(37, 37)
-        };
+          };
 
-        var marker = new google.maps.Marker({
-          map: map,
-          position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-          animation: google.maps.Animation.DROP,
-          icon: image
+          var marker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            animation: google.maps.Animation.DROP,
+            icon: image
+          });
+
+          $scope.onCreate({map: map});
+
+          // Stop the side bar from dragging when mousedown/tapdown on the map
+          google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+            e.preventDefault();
+            return false;
+          });
         });
+      }
 
-        $scope.onCreate({map: map});
-
-        // Stop the side bar from dragging when mousedown/tapdown on the map
-        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
-          e.preventDefault();
-          return false;
-        });
-      });
-    }
-
-    if (document.readyState === "complete") {
-      initialize();
-    } else {
-      google.maps.event.addDomListener(window, 'load', initialize);
+      if (document.readyState === "complete") {
+        initialize();
+      } else {
+        google.maps.event.addDomListener(window, 'load', initialize);
       }
     }
   }
